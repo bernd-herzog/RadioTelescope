3c6e0ffa905bc505bc64af8ef53d18236c2dc06c
Bernd Herzog <bernd@nachregenkommtsonne.de>
2019-04-04 20:03:13 +0200

    added app

---
+++ Radio.Telescope.App/app/src/main/java/de/nachregenkommtsonne/rf/telescope/MainActivity.java
@@ -0,0 +1,104 @@
+package de.nachregenkommtsonne.rf.telescope;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "Done", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    543);
+
+        }
+
+
+        String[] stringArrayList = Environment.getExternalStorageDirectory().list();
+
+        final List<String> result = new ArrayList<>();
+
+        for (String entry : stringArrayList) {
+            if (entry.endsWith("tbz"))
+                result.add(entry);
+        }
+
+        ListView myListView = (ListView) findViewById(R.id.listview_activity_main);
+
+        ArrayAdapter<String> myArrayAdapter =
+                new ArrayAdapter(
+                        this, // Die aktuelle Umgebung (diese Activity)
+                        R.layout.list_row, // Die ID des Zeilenlayouts (der XML-Layout Datei)
+                        R.id.quote_text,   // Die ID eines TextView-Elements im Zeilenlayout
+                        result);  // Beispieldaten in einer ArrayList
+
+        myListView.setAdapter(myArrayAdapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String file = result.get(position);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
